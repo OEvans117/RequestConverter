3de8618674fe7ffb78cb094f02dbf15bba242f53
FrenziedIM <oscarppevans@gmail.com>
2023-01-20 17:47:44 +0100

    All

--- RequestConverterAngular/src/app/components/mainpage/mainpage.component.ts
+++ RequestConverterAngular/src/app/components/mainpage/mainpage.component.ts
@@ -21,7 +21,5 @@ import { Validators } from '@angular/forms';
 export class MainpageComponent {
   constructor(public rcApi: RcapiService, private route: ActivatedRoute, private location: Location) {
     const regex = /\/r\/(\b\w{6,6})\b/gm;
-    console.log(this.route.snapshot.paramMap.get('id'));
-
   }
 }
